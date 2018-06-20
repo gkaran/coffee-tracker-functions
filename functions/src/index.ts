c2a47289c74c4f9b6e829e9ca465d7b550b52f03
gkaran <gkaran89@gmail.com>
2018-06-20 11:54:34 -0400

    (feature) split functions for easier reading

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,16 +2,95 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-export const portionsSum = functions.firestore
+// ========================================================================
+//                        USER PORTION HANDLING
+// ========================================================================
+
+export const update_user_portions_on_portion_creation = functions.firestore
     .document('/portions/{portionid}')
-    .onWrite(change => updateUserStats(change, 'totalPortions', 'amount'));
+    .onCreate((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = change.get('amount') || 0;
+        return update_user_total_portions(context.eventId, userRef, userId, increment);
+    });
 
-export const paidPortionsSum = functions.firestore
+export const update_user_portions_on_portion_update = functions.firestore
+    .document('/portions/{portionid}')
+    .onUpdate((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = (change.after.get('amount') || 0) - change.before.get('amount') || 0;
+        return update_user_total_portions(context.eventId, userRef, userId, increment);
+    });
+
+export const update_user_portions_on_portion_deletion = functions.firestore
+    .document('/portions/{portionid}')
+    .onDelete((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = change.get('amount') || 0;
+        return update_user_total_portions(context.eventId, userRef, userId, -increment);
+    });
+
+function update_user_total_portions(eventId: string, userRef: FirebaseFirestore.DocumentReference, userId: string, increment: number) {
+    return db.runTransaction(transaction => transaction.get(userRef).then(user => {
+        const oldPortions = user.get('totalPortions') || 0;
+        const newPortions = oldPortions + increment;
+        console.log(`${eventId}: Updating total portions from ${oldPortions} to ${newPortions} for user ${userId}`);
+        return transaction.update(userRef, {totalPortions: newPortions});
+    }));
+}
+
+
+// ========================================================================
+//                        USER PAYMENT HANDLING
+// ========================================================================
+
+export const update_user_payments_on_payment_creation = functions.firestore
     .document('/payments/{paymentid}')
-    .onWrite(change => updateUserStats(change, 'paidPortions', 'portions'));
+    .onCreate((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = change.get('portions') || 0;
+        return update_user_total_paid_portions(context.eventId, userRef, userId, increment);
+    });
 
-export const initUserData = functions.auth.user().onCreate((user) => {
+export const update_user_payments_on_payment_update = functions.firestore
+    .document('/payments/{paymentid}')
+    .onUpdate((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = (change.after.get('portions') || 0) - change.before.get('portions') || 0;
+        return update_user_total_paid_portions(context.eventId, userRef, userId, increment);
+    });
+
+export const update_user_payments_on_payment_deletion = functions.firestore
+    .document('/payments/{paymentid}')
+    .onDelete((change, context) => {
+        const userId = context.auth.uid;
+        const userRef = db.doc(`/users/${userId}`);
+        const increment = change.get('portions') || 0;
+        return update_user_total_paid_portions(context.eventId, userRef, userId, -increment);
+    });
+
+function update_user_total_paid_portions(eventId: string, userRef: FirebaseFirestore.DocumentReference, userId: string, increment: number) {
+    return db.runTransaction(transaction => transaction.get(userRef).then(user => {
+        const oldPortions = user.get('paidPortions') || 0;
+        const newPortions = oldPortions + increment;
+        console.log(`${eventId}: Updating paid portions from ${oldPortions} to ${newPortions} for user ${userId}`);
+        return transaction.update(userRef, {paidPortions: newPortions});
+    }));
+}
+
+
+// ========================================================================
+//                        USER CREATION HANDLING
+// ========================================================================
+
+export const init_user_data = functions.auth.user().onCreate((user) => {
     return admin.firestore().doc(`/users/${user.uid}`).set({
         totalPortions: 0,
         paidPortions: 0,
@@ -19,24 +98,3 @@ export const initUserData = functions.auth.user().onCreate((user) => {
         name: user.displayName || user.email
     }, {merge: true})
 });
-
-function updateUserStats(change, userFieldForUpdate, collectionAmountField) {
-    let increment = 0;
-    if (change.after.exists && change.before.exists) { // UPDATE
-        increment = change.after.get(collectionAmountField) - change.before.get(collectionAmountField)
-    } else if (change.after.exists && !change.before.exists) { // CREATE
-        increment = change.after.get(collectionAmountField);
-    } else if (!change.after.exists && change.before.exists) { // DELETE
-        increment = -change.before.get(collectionAmountField);
-    }
-
-    const userRef = admin.firestore().doc(`/users/${change.after.get('user')}`);
-    console.log(`Amount to be added: ${increment}`);
-    return userRef
-        .get()
-        .then(user => {
-            console.log(`Current value of ${userFieldForUpdate}: ${user.get(userFieldForUpdate)}`);
-            return userRef.set({[userFieldForUpdate]: user.get(userFieldForUpdate) + increment}, {merge: true})
-        });
-}
-
